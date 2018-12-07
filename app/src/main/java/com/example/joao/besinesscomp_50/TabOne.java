16a6aa898f73bd5918e7875fe2375676128ca474
Joao Paulo <joaojuvencio87@gmail.com>
2018-12-07 14:56:26 -0200

    Templete

---
+++ app/src/main/java/com/example/joao/besinesscomp_50/TabOne.java
@@ -0,0 +1,20 @@
+package com.example.joao.besinesscomp_50;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabOne extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+}
